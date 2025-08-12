5bf9f34adf5cf2ed826bdcab726a6dd6c253d8b9
dariusjvc <dariusjvc@MSI.localdomain>
2025-08-13 01:05:41 +0200

    Added the first version of OFTWithRateLimit.sol

---
+++ contracts/OFTWithRateLimit.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@layerzerolabs/solidity-examples/contracts/token/oft/v2/OFTV2.sol";
+
+contract OFTWithRateLimit is Ownable, OFTV2 {
+    uint256 public dailyLimit;
+    uint256 public spentToday;
+    uint256 public lastReset;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _sharedDecimals,
+        address _lzEndpoint,
+        uint256 _dailyLimit
+    ) OFTV2(_name, _symbol, _sharedDecimals, _lzEndpoint) {
+        dailyLimit = _dailyLimit;
+        lastReset = block.timestamp;
+    }
+
+    function _debitFrom(
+        address _from,
+        uint16 _dstChainId,
+        bytes32 _toAddress,
+        uint _amount
+    )
+        internal
+        override
+        returns (uint256 amountDebitedLD)
+    {
+        // reset diario
+        if (block.timestamp > lastReset + 1 days) {
+            spentToday = 0;
+            lastReset = block.timestamp;
+        }
+
+        require(spentToday + _amount <= dailyLimit, "Daily limit exceeded");
+        spentToday += _amount;
+
+        // capturamos y devolvemos el único uint que retorna el padre
+        amountDebitedLD = super._debitFrom(_from, _dstChainId, _toAddress, _amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner {
+    _mint(_to, _amount);
+}
+
+}
